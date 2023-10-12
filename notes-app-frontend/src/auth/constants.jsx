dbc7592fbed1253ee5635ab44bf52275ceebc4db
YulzCastro <andreayulitzacastrogomez88@gmail.com>
2023-10-12 10:29:35 -0500

    se cambia url del backend

--- notes-app-frontend/src/auth/constants.jsx
+++ notes-app-frontend/src/auth/constants.jsx
@@ -1,2 +1,2 @@
-//export const API_URL = "https://notes-app-backend-67f2.onrender.com"
-export const API_URL = "http://localhost:3000"
+export const API_URL = "https://notes-app-backend-67f2.onrender.com"
+//export const API_URL = "http://localhost:3000"
